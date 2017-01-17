e36091bbad9b64eb52d031c1a62036168f4fd4fd
Iana Andrychowicz <filonenko.jana@gmail.com>
2017-01-17 14:23:44 -0800

    eoor handle & sisplay msg when cell already taken

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 angular.module('ticTacToe', [])
 .controller('Ctrl', ($scope) => {
-  $scope.player1 = 'X';
-  $scope.player2 = 'O';
-  $scope.move = $scope.player1;
-  $scope.makeMove = function (move, index, row) {
-    row[index] = move;
-    $scope.move = move == $scope.player1 ? $scope.player2 : $scope.player1;
-  };
   $scope.board = [
     ['--', '--', '--'],
     ['--', '--', '--'],
     ['--', '--', '--'],
   ];
-})
-;
+  $scope.player1 = 'X';
+  $scope.player2 = 'O';
+  $scope.move = $scope.player1;
+  $scope.error = '';
+  $scope.makeMove = function (move, index, row) {
+    if (row[index] === '--') {
+      row[index] = move;
+      $scope.move = move == $scope.player1 ? $scope.player2 : $scope.player1;
+      $scope.error = '';
+    } else {
+      $scope.error = 'please make a move on an empty cell';
+    }
+  };
+});
