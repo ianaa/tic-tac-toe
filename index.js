a1b8508ef6b440c2d01156589d3f7125610e8104
Iana Andrychowicz <filonenko.jana@gmail.com>
2017-01-17 14:16:34 -0800

    Add ng-reapeat to build a board, add func to make moves

--- index.js
+++ index.js
@@ -2,5 +2,15 @@ angular.module('ticTacToe', [])
 .controller('Ctrl', ($scope) => {
   $scope.player1 = 'X';
   $scope.player2 = 'O';
+  $scope.move = $scope.player1;
+  $scope.makeMove = function (move, index, row) {
+    row[index] = move;
+    $scope.move = move == $scope.player1 ? $scope.player2 : $scope.player1;
+  };
+  $scope.board = [
+    ['--', '--', '--'],
+    ['--', '--', '--'],
+    ['--', '--', '--'],
+  ];
 })
 ;
