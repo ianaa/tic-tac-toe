760570a31c898ef69223e4022855bc4233ee20c2
Iana Andrychowicz <filonenko.jana@gmail.com>
2017-01-17 16:06:51 -0800

    cleanup

--- index.js
+++ index.js
@@ -1,34 +1,24 @@
 angular.module('ticTacToe', [])
 .controller('Ctrl', ($scope) => {
-  // $scope.board = [
-  //   ['--', '--', '--'],
-  //   ['--', '--', '--'],
-  //   ['--', '--', '--'],
-  // ];
-  // $scope.move = $scope.player1;
-  // $scope.error = '';
   $scope.player1 = 'X';
   $scope.player2 = 'O';
+
   $scope.makeMove = function (move, index, row) {
-    if (row[index] === '--') {
+    if ($scope)
+      {if (row[index] === '--') {
       row[index] = move;
       const state = $scope.checkWin(row, index);
       $scope.error = state ? state == 1 ? `We have a winner!!! Conrats ${$scope.move}` : 'We have a draw!' : '';
       $scope.move = move == $scope.player1 ? $scope.player2 : $scope.player1;
     } else {
       $scope.error = 'please make a move on an empty cell';
-    }
+    }}
   };
   $scope.checkWin = function (row, index) {
-    console.log('row number', $scope.board.indexOf(row));
-
-    if (row.every(cell => cell === row[index])) {
+    if (row.every(cell => cell === row[index]) || $scope.board.every(item => item[index] === row[index])) {
       return 1;
     }
-    if ($scope.board.every(item => item[index] === row[index])) {
-      return 1;
-    }
-    if ([$scope.board[0][0], $scope.board[1][1], $scope.board[2][2]].every((item) => item === row[index])
+    if ([$scope.board[0][0], $scope.board[1][1], $scope.board[2][2]].every(item => item === row[index])
     || [$scope.board[0][2], $scope.board[1][1], $scope.board[2][0]].every(item => item === row[index])) {
       return 1;
     }
